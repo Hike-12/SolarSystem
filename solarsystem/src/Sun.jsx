9b1a5003f49fe435d9147188eddd02802a0ea271
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2026-06-07 16:57:19 +0530

    removed weird glow around sun

--- solarsystem/src/Sun.jsx
+++ solarsystem/src/Sun.jsx
@@ -20,7 +20,7 @@ const Sun = ({ position = [0, 0, 0], onClick, timeSpeed = 1 }) => {
       sunRef.current.rotation.y += values.sunRotationSpeed * timeSpeed; // Slow rotation
       
       // Create subtle pulsing effect for emissive intensity
-      const pulse = Math.sin(clock.getElapsedTime() * 0.5) * 0.2 + 1.3;
+      const pulse = Math.sin(clock.getElapsedTime() * 0.5) * 0.3 + 2.0;
       sunRef.current.material.emissiveIntensity = pulse;
     }
   });
@@ -31,7 +31,7 @@ const Sun = ({ position = [0, 0, 0], onClick, timeSpeed = 1 }) => {
             onClick("Sun"); // This passes the planet name to the handler
           }}>
       {/* Sun glow effect - increased intensity */}
-      <pointLight position={[0, 0, 0]} intensity={2.5} color="#FFF9E0" distance={100} />
+      <pointLight position={[0, 0, 0]} intensity={6} color="#FFF9E0" distance={100} />
       
       {/* Sun mesh with texture */}
       <Sphere ref={sunRef} args={[values.sunSize, 64, 64]}>
@@ -43,24 +43,6 @@ const Sun = ({ position = [0, 0, 0], onClick, timeSpeed = 1 }) => {
           normalScale={[0.05, 0.05]}
           roughness={0.6}
           metalness={0.3}
-        />
-      </Sphere>
-      
-      {/* Additional outer glow effect - increased opacity */}
-      <Sphere args={[values.sunSize * 1.2, 32, 32]}>
-        <meshBasicMaterial 
-          color="#FF4500" 
-          transparent={true} 
-          opacity={0.2} // Increased from 0.15
-        />
-      </Sphere>
-      
-      {/* Inner corona for extra detail */}
-      <Sphere args={[values.sunSize * 1.05, 32, 32]}>
-        <meshBasicMaterial 
-          color="#FFF9E0" 
-          transparent={true} 
-          opacity={0.3}
         />
       </Sphere>
     </group>
